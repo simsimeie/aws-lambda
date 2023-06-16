d37a6962c47fa8d05b51ea9b8fd3149a6c06d71b
simsimeie <simsimeie@naver.com>
2023-06-17 01:02:47 +0900

    pathvariable로 rds 조회하여 redirect하는 예제 개발

--- src/main/java/org/example/PathVarHandler.java
+++ src/main/java/org/example/PathVarHandler.java
@@ -4,30 +4,47 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PathVarHandler implements RequestHandler<Map<String,Object>, APIGatewayProxyResponseEvent> {
-
+    private static final String URL = "jdbc:postgresql://database-2.cbs7c8887131.ap-northeast-2.rds.amazonaws.com:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
     @Override
     public APIGatewayProxyResponseEvent handleRequest(Map<String, Object> input, Context context) {
         Map<String,String> pathParameters = (Map<String,String>) input.get("pathParameters");
         String path = "basic";
-        String redirectLocation = "https://google.com";
+        String redirectLocation = "https://naver.com";
 
         if (pathParameters != null) {
             context.getLogger().log("Path variable " + pathParameters.get("data"));
             path = pathParameters.get("data");
         }
 
-        if(path.equals("abc")){
-            redirectLocation = "https://openai.com";
-        }
-        else if (path.equals("def")){
-            redirectLocation = "https://amazon.com";
-        }
+        String selectSQL = "SELECT origin_url FROM url WHERE short_url = ?";
 
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            try (PreparedStatement preparedStatement = conn.prepareStatement(selectSQL)) {
+                preparedStatement.setString(1, path);
+                try (ResultSet rs = preparedStatement.executeQuery()) {
+                    while (rs.next()) {
+                        // 이 부분에 각 컬럼에 대한 처리를 넣어주세요.
+                        // 예를 들어, String data = rs.getString("columnName");
+                        redirectLocation = rs.getString("origin_url");
+                        context.getLogger().log("## 쿼리조회성공 " + redirectLocation);
+
+                    }
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
 
         context.getLogger().log("Success #1 " + LocalDateTime.now());
         // 리디렉션 응답 생성
